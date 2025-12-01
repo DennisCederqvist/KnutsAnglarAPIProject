64b93a1d6d24f90629f9bd7a77629a8ef1d0afc4
oliviaaMach <olivia.mach@outlook.com>
2025-12-01 09:21:31 +0100

    added pictures

--- weatherCode.js
+++ weatherCode.js
@@ -1,20 +1,28 @@
 export function weatherCode(weatherDescription) {
     let description;
+    let background;
 
     if (weatherDescription === 0) {
         description = "☀️ Klar himmel"; 
+        background = "./imgs/sun.jpg";
     } else if (weatherDescription >= 1 && weatherDescription <= 3) {
         description = "☁️ Molnig"; 
+        background = "./imgs/cloud.jpg";
     } else if (weatherDescription === 45 || weatherDescription === 48) {
         description = "🌫️ Dimma"; 
+        background = "./imgs/fog.jpg";
     } else if (weatherDescription === 51 || weatherDescription === 53 || weatherDescription === 55) {
         description = "🌧️ Duggregn"; 
+        background = "./imgs/rain.jpg";
     } else if (weatherDescription === 61 || weatherDescription === 63 ||weatherDescription === 65) {
         description = "🌧️ Regn"; 
+        background = "./imgs/rain.jpg";
     } else if (weatherDescription === 71 || weatherDescription === 73 || weatherDescription === 75) {
         description = "🌨️ Snö"; 
+        background = "./imgs/snow.jpg";
     } else if (weatherDescription === 95 || weatherDescription === 96 || weatherDescription === 99) {
         description = "🌩️ Blixt och dunder";
+        background = "./imgs/thunder.jpg";
     } else {
         return null;
     }
